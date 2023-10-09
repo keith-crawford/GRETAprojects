af5df0050a138fac0979acc5259c91081944bb58
keith-crawford@thinjetty.com <keith-crawford@thinjetty.com>
2023-10-09 17:59:13 +0100

    challenge_4.py

--- challenge_4.py
+++ challenge_4.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python # pylint: disable=missing-module-docstring
+# -*- coding: utf-8 -*-
+# ******************************************************************************************************************120
 # Challenge 4: Make some flask APIs do what you want
 # These is a production grade Apache 2.0 webserver running on port 80
 # it serves your static folder all the time
@@ -32,4 +35,72 @@
 # 4. add a GET end point which generates an image and returns an image.
 # Extra Credit
 # Work out how to make a simple webpage that calls an api...
+# ******************************************************************************************************************120
 
+# standard imports
+
+import json
+
+# 3rd party imports
+import click
+import requests
+
+# custom imports
+
+
+# declare an exception type so pylint doesn't tell you aren't catching specific enough exceptions.
+class MyAPIValidationException(Exception):
+    """When response validation fails"""
+
+    def __init__(self, url: str, response, payload: dict = None):
+        if payload is None:
+            payload = {}
+        self.url = url
+        self.response = response
+        self.payload = payload
+        self.message = f'Did not receive 200 from url: {url} {self.response.status_code} {self.response.text}'
+        super().__init__(self.message)
+
+
+# declare a global constant for timeout.
+TIMEOUT = 5000
+
+headers ={}
+baseurl = "https://api.parliament.uk/historic-hansard/sittings/"
+year = (int)
+month = (str)
+date = (int)
+@click.command()
+@click.option('-y', '--year', type=int, required=True, default='2002', help='Four figure integer, eg. 2002')
+@click.option('-m', '--month', type=str, required=True, default="apr", help='jan/feb/mar/apr/may/jun/jul/aug/sep/oct/nov/dec')
+@click.option('-d', '--date', type=int, required=True, default=16, help='1-31')
+
+def main(year: int, month: str, date: int) -> None:
+    print(get_hansard_report(headers, baseurl, year, month, date))
+
+def get_hansard_report(headers: dict, baseurl:str, year: int, month: str, date: int) -> dict:
+    """Does a get of this end point
+    https://api.parliament.uk/historic-hansard/sittings/"""
+
+    #Example https://api.parliament.uk/historic-hansard/sittings/2002/apr/16
+
+    url = f'{baseurl}/{year}/{month}/{date}'
+
+    # define a payload to send if it's a post
+
+    payload = {}
+
+    response = requests.request(
+        "GET", url, headers=headers, data=json.dumps(payload), timeout=TIMEOUT)
+    return _validate_response(response, url)
+
+def _validate_response(response: requests.Response, url: str, field: str = None, payload: dict = None):
+    """Validate the response from the API and provide consistent error handling"""
+    if payload is None:
+        payload = {}
+    if isinstance(response, str) or response.status_code != 200:
+        raise MyAPIValidationException(
+            url=url, payload=payload, response=response)
+
+if __name__ == '__main__':
+    main()  # pylint: disable=no-value-for-parameter
