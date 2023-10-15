98c73b71623ccbc7a1f24f69aa9d399a286bfe5a
keith-crawford@thinjetty.com <keith-crawford@thinjetty.com>
2023-10-15 16:50:17 +0100

    Adding requests example

---
+++ request_example.py
@@ -0,0 +1,31 @@
+import requests
+
+# get apikey from a protected file
+apikey = open('.creds',mode='r',encoding='utf8').read()
+print(apikey)
+
+# do it for whoever
+stock_ticker = 'IBM'
+
+# some FMP apikey you've sorted out - see the parameters
+url = f'https://financialmodelingprep.com/api/v3/profile/{stock_ticker}?apikey={apikey}'
+
+# Make a response object
+response = requests.request(method="GET",url=url,timeout=10)
+
+# Inspect it
+print(response)
+print(type(response))
+
+# Print the return code
+# https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+print('\nResponse Code: https://developer.mozilla.org/en-US/docs/Web/HTTP/Status')
+print(response.status_code)
+
+# Print the text if there is any
+print('\nText is a text representation of the JSON here******************')
+print(response.text)
+
+# Print the json if there is any
+print('\nJSON is the actual thing because FMP plays nice with json*******')
+print(response.text)
